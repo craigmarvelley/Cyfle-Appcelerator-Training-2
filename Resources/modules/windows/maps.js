3639875326a9942463fe51b6aa45634ad9b15c82
Craig Marvelley <craig.marvelley@gmail.com>
2011-12-06 16:26:30 +0000

    Added map window

--- Resources/modules/windows/maps.js
+++ Resources/modules/windows/maps.js
@@ -1,14 +1,57 @@
 exports.MapWindow = function () {
 	
-	var label = Ti.UI.createLabel({
-		text: 'Map'
-	});
-	
 	var window = Ti.UI.createWindow({
-		backgroundColor: '#FFF'
+		backgroundColor: '#FFF',
+		title: 'Maps',
+		layout: 'vertical'
 	});
 	
-	window.add(label);
+	var loadButton = Ti.UI.createButton({
+		title: 'Load',
+		width: 100,
+		height: 30
+	});
+	
+	var toolbar;
+	if(Ti.Platform.name == 'android') {
+		toolbar = Ti.UI.createView({
+			layout: 'horizontal',
+			height: 44
+		});
+		
+		toolbar.add(loadButton);
+	}
+	else {
+		toolbar = Ti.UI.createToolbar({
+			height: 44,
+			zIndex: 10,
+			items: [
+				loadButton
+			]
+		});
+	}
+	
+	var annotation = Ti.Map.createAnnotation({
+		latitude: 51.47828,
+	    longitude: -3.18243
+	});
+	
+	var mapview = Ti.Map.createView({
+		region: {
+			latitude: 51.47828,
+			longitude: -3.18243,
+			latitudeDelta: 0.01,
+			longitudeDelta: 0.01			
+		},
+		animate:true,
+    	regionFit:true,
+    	width: 320,
+    	height: 400,
+    	annotations: [annotation]
+	});
+
+	window.add(toolbar);
+	window.add(mapview);
 	
 	return window;
 	
