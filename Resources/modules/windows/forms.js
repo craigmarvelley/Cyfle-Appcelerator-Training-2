feb02f895f1ca660577c5d01285a08261bce3126
Craig Marvelley <craig.marvelley@gmail.com>
2011-12-06 13:08:37 +0000

    Added android density support, and implemented form window

--- Resources/modules/windows/forms.js
+++ Resources/modules/windows/forms.js
@@ -1,14 +1,87 @@
-exports.FormWindow = function () {
+function createFormAndAddToWindow(window) {
 	
 	var label = Ti.UI.createLabel({
-		text: 'Form'
+		top: 10,
+		left: 10,
+		right: 10,
+		height: 20,
+		text: 'Form',
+		color: 'red',
+		font: {
+			fontSize: 16,
+			fontWeight: 'bold'
+		}
 	});
 	
-	var window = Ti.UI.createWindow({
-		backgroundColor: '#FFF'
+	var textfieldHeight = 30;
+	if(Ti.Platform.name == 'android') {
+		textfieldHeight = 50;
+    }
+	
+	var textfield = Ti.UI.createTextField({
+		top: 10,
+		left: 10,
+		right: 10,
+		hintText: 'Name',
+		height: textfieldHeight 
+	});
+	
+	var addressLabel = Ti.UI.createLabel({
+		top: 10,
+		left: 10,
+		right: 10,
+		height: 20,
+		text: 'Address',
+		font: {
+			fontSize: 14,
+			fontWeight: 'bold'
+		}
+	});
+	
+	var textarea = Ti.UI.createTextArea({
+		top: 10,
+		left: 10,
+		right: 10,
+		height: 60,
+		editable: true
+	});
+	
+	var button = Ti.UI.createButton({
+		top: 10,
+		title: 'Click me!',
+		width: 100,
+		height: 30
 	});
 	
 	window.add(label);
+	window.add(textfield);
+	window.add(addressLabel);
+	window.add(textarea);
+	window.add(button);
+	
+	button.addEventListener('click', function () {
+		
+		var message = 'Hello ' + textfield.value + '\nYour address is: ' + textarea.value;
+		
+		var alert = Ti.UI.createAlertDialog({
+			message: message
+		});
+		
+		alert.show();
+		
+	});
+	
+}
+
+exports.FormWindow = function () {
+	
+	var window = Ti.UI.createWindow({
+		title: 'Forms',
+		backgroundColor: '#666',
+		layout: 'vertical'
+	});
+	
+	createFormAndAddToWindow(window);
 	
 	return window;
 	
