1e0bd2454ad25ee1911459ce9f8ef9685431547e
Craig Marvelley <craig.marvelley@gmail.com>
2011-12-06 15:07:17 +0000

    Added remote webview browser example

--- Resources/modules/windows/webviews.js
+++ Resources/modules/windows/webviews.js
@@ -1,14 +1,101 @@
 exports.WebViewWindow = function () {
 	
-	var label = Ti.UI.createLabel({
-		text: 'Webview'
-	});
-	
 	var window = Ti.UI.createWindow({
-		backgroundColor: '#FFF'
+		title: 'Webviews',
+		backgroundColor: '#FFF',
+		layout: 'vertical'
 	});
 	
-	window.add(label);
+	var backButton = Ti.UI.createButton({
+		title: 'Back',
+		width: 30,
+		font: {
+			fontSize: 12
+		}
+	});
+	
+	var forwardButton = Ti.UI.createButton({
+		title: 'Forward',
+		width: 30,
+		font: {
+			fontSize: 12
+		}
+	});
+	
+	var urlField = Ti.UI.createTextField({
+		top: 5,
+		hintText: 'Enter a URL',
+		height: 20,
+		width: 180,
+		value: 'http://www.google.com',
+		font: {
+			fontSize: 12
+		},
+		autocapitalization: Ti.UI.TEXT_AUTOCAPITALIZATION_NONE
+	});
+	
+	var goButton = Ti.UI.createButton({
+		title: 'Go',
+		width: 30,
+		font: {
+			fontSize: 12
+		}
+	});
+	
+	var toolbar;
+	
+	if(Ti.Platform.name == 'android') {
+		urlField.setHeight(30);
+		
+		toolbar = Ti.UI.createView({
+			layout: 'horizontal',
+			height: 30
+		});
+		
+		toolbar.add(backButton);
+		toolbar.add(forwardButton);
+		toolbar.add(urlField);
+		toolbar.add(goButton);
+	}
+	else {
+		urlField.borderStyle = Titanium.UI.INPUT_BORDERSTYLE_ROUNDED;
+		
+		toolbar = Ti.UI.createToolbar({
+			height: 20,
+			items: [
+				backButton,
+				forwardButton,
+				urlField,
+				goButton
+			]
+		});
+	}
+	
+	var webview = Ti.UI.createWebView({
+		top: 5,
+		width: 'auto',
+		height: 'auto',
+		url: 'http://www.google.com'
+	});
+	
+	window.add(toolbar);
+	window.add(webview);
+	
+	backButton.addEventListener('click', function() {
+		if(webview.canGoBack()) {
+			webview.goBack();
+		}
+	});
+	
+	forwardButton.addEventListener('click', function() {
+		if(webview.canGoForward()) {
+			webview.goForward();
+		}
+	});
+	
+	goButton.addEventListener('click', function(e) {
+		webview.url = urlField.value;
+	});
 	
 	return window;
 	
