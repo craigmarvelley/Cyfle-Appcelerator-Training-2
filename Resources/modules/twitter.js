2921cadca887d94ce39da9e18992b4c781fbaea1
Craig Marvelley <craig.marvelley@gmail.com>
2011-12-07 15:31:48 +0000

    Save tweets to local database

--- Resources/modules/twitter.js
+++ Resources/modules/twitter.js
@@ -3,10 +3,10 @@ var database = Ti.Database.install('twitter.sqlite', 'twitter');
 /* PUBLIC */
 
 exports.loadStoredTweets = function () {
-	
+
 	var tweets = [];
 
-	// LOAD TWEETS FROM DATABASE HERE
+	// LOAD STORED TWEETS HERE
 	
 	return tweets;
 
@@ -21,7 +21,16 @@ exports.loadNewTweets = function (callback) {
 	httpClient.onload = function() {
 		var tweets = JSON.parse(this.responseText);
 		
-		// SAVE TWEETS TO DATABASE HERE
+		var sql = "INSERT OR REPLACE INTO tweets (id, tweet, created_at)"
+			        + " VALUES (?, ?, ?)";
+		
+		for(var i=0, n=tweets.length; i<n; i++) {
+			
+			var tweet = tweets[i];
+			
+			database.execute(sql, tweet.id, JSON.stringify(tweet), tweet.created_at);
+			
+		}
 		
 		callback.call(this, tweets);
 	}
